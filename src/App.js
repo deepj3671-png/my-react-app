e0b51be03fb65ac24772ca08a098db2010b84229
deepj3671-png <deepj3671@gmail.com>
2026-02-26 14:29:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 style={{ color: 'blue', textAlign: 'center' }}>Deep Joshi</h1>
+      <p>My first React app is working</p>
+    </div>
+  );
+}
+
+export default App;
