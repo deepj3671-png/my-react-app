02fad8ab40a7cffb5e34003bed9956ee53dae7fb
deepj3671-png <deepj3671@gmail.com>
2026-02-26 13:26:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1 style={{ color: 'blue', textAlign: 'center' }}>Deep Joshi</h1>
       <p>My first React app is working</p>
     </div>
   );
